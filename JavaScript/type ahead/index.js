84d29a25e1c03bece9584fb22b11f7bdee76b73f
Andrés Felipe Martínez Martínez <afmartinez099@misena.edu.co>
2021-09-23 13:54:54 -0500

    avance forma de pago, workout

--- JavaScript/type ahead/index.js
+++ JavaScript/type ahead/index.js
@@ -6,7 +6,7 @@ fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
 
-function findMatches(wordToMatch, cities) {
+function findMatches(wordToMatch) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi')
         return place.city.match(regex) || place.state.match(regex) ;
@@ -16,4 +16,26 @@ function findMatches(wordToMatch, cities) {
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+        <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
---
+++ Workouts/3 Subir extracto bancario/readMe.md
@@ -0,0 +1,15 @@
+This excercise requires DOM manipulation and Javascript fundamentals to create a bank extract uploader component
+
+Requirement #1 - appearing and disappearing file uploader option
+
+Requirement #2 - dynamic select with bank list
+
+Requirement #3 - Validations
+
+Este ejercicio requiere la manipulación de DOM y los fundamentos de Javascript para crear un componente de carga de extractos bancarios
+
+Requisito #1: opción de carga de archivos que aparecen y desaparecen
+
+Requisito #2: selección dinámica con lista de bancos
+
+Requisito #3 - Validaciones
