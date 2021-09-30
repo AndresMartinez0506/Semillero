a6c5b885e65ff5ab73869dc3c9865107ea0117e0
Andrés Felipe Martínez Martínez <afmartinez099@misena.edu.co>
2021-09-30 14:02:17 -0500

    Primeras funcionalidades risk

--- JavaScript/4 Risk Configuartions/src/script.js
+++ JavaScript/4 Risk Configuartions/src/script.js
@@ -41,11 +41,72 @@ pEncabezado.classList.add('pEncabezado');
 const botonEncabezado = document.createElement('button');
 botonEncabezado.id = "botonEncabezado";
 botonEncabezado.type = "submit";
-botonEncabezado.innerHTML = "Nuevo"
+botonEncabezado.innerHTML = "Nuevo";
+
+const formulario = document.createElement('form');
+formulario.classList.add('formulario');
+const divAzul = document.createElement('div');
+divAzul.classList.add('azul');
+const equis = document.createElement('button');
+equis.innerHTML = "X";
+equis.classList.add('equis');
+
+
+const abajoForm = document.createElement('div');
+abajoForm.classList.add('abajoForm');
+
+const divDescripcion = document.createElement('div');
+divDescripcion.classList.add('divDescripcion');
+
+const labelDescripcion = document.createElement('label');
+labelDescripcion.innerHTML = "Descripción";
+labelDescripcion.classList.add('labelDescripcion');
+
+const inputDescripcion = document.createElement('input');
+inputDescripcion.classList.add('inputDescripcion');
+
+const divMinimo = document.createElement('div');
+divMinimo.classList.add('divMinimo');
+
+const labelValorMinimo = document.createElement('label');
+labelValorMinimo.innerHTML = "Valor Mínimo";
+labelValorMinimo.classList.add('labelValorMinimo');
+
+const inputValorMinimo = document.createElement('input');
+inputValorMinimo.classList.add('inputValorMinimo');
+
+const divMaximo = document.createElement('div');
+divMaximo.classList.add('divMaximo');
+
+const labelValorMaximo = document.createElement('label');
+labelValorMaximo.innerHTML = "Valor Máximo";
+labelValorMaximo.classList.add('labelValorMaximo');
+
+const inputValorMaximo = document.createElement('input');
+inputValorMaximo.classList.add('inputValorMaximo');
+
+const botonGuardar = document.createElement('button');
+botonGuardar.id = "botonGuardar";
+botonGuardar.type = "submit";
+botonGuardar.innerHTML = "Guardar";
 
 body.append(main);
-main.append(titulo, abajo);
+main.append(titulo, abajo, formulario);
+formulario.append(divAzul, abajoForm);
+abajoForm.append(divDescripcion, divMinimo, divMaximo, botonGuardar);
+divDescripcion.append(labelDescripcion, inputDescripcion);
+divMinimo.append(labelValorMinimo, inputValorMinimo);
+divMaximo.append(labelValorMaximo, inputValorMaximo);
+divAzul.append(equis)
 abajo.append(divInputs, tabla);
 tabla.append(divEncabezado);
 divEncabezado.append(pEncabezado, botonEncabezado)
 divInputs.append(inputImpactos, inputProbabilidades, inputRiesgos, inputEvaluacion);
+
+botonEncabezado.addEventListener("click", ()=> {
+    formulario.style.display = "block";
+});
+
+equis.addEventListener("click", ()=> {
+    formulario.style.display = "none";
+})
---
+++ JavaScript/dibujar - copia/js/funCanvas.js
@@ -0,0 +1,3 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
